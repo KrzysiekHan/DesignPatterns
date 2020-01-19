5db35798bfa7085b123bda8da45e2c712f1dc5fa
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-01-19 23:21:04 +0100

    adapter pattern thoughts

--- AdapterPattern/Program.cs
+++ AdapterPattern/Program.cs
@@ -4,6 +4,15 @@ namespace AdapterPattern
 {
     internal static class Program
     {
+        // Definicja książkowa
+        //Tworzymy klasę adapter która implementuje interfejs IDuck, a indyka wstrzykujemy do niej przez konstruktor.
+        //Dzięki takiemu zabiegowi można w klasie która z definicji jest kaczką (implementuje interfejs IDuck) podmienić zachowanie kwakania na 
+        //gęganie :) 
+        //public void Quack()
+        //{
+        //    _turkey.Gobble();
+        //}
+
         private static void Main()
         {
             var turkey = new WildTurkey();
